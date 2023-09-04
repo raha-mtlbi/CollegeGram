0646658ea2778dd5f9feaabee717a8277a7bb3d4
Mohammadreza <109216626+MohammadrezaFarahmand@users.noreply.github.com>
2023-09-04 16:32:19 +0330

    develop the left sidebar and the routes

---
+++ src/component/NotificationPage.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import SideBar from "./sidebar"
+
+const NotificationPage = () => {
+  return (
+    <div className="flex justify-between items-center mt-32 w-full">
+      <div className="w-full">notification</div>
+      <div>
+        <SideBar />
+      </div>
+    </div>
+  )
+}
+
+export default NotificationPage
