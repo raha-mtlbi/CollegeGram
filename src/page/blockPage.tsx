1f4efaac0ea49b5605cd303b597285d80d62c146
raha.mtlbi <raha.mtlbi@gmail.com>
2023-09-06 02:05:21 +0330

    blockPage design1

---
+++ src/page/blockPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const BlockPage = () => {
+  return (
+    <div className="text-center	justify-center">
+      <p className="text-[20px] font-bold text-[#17494D]">
+        مثل اینکه بلاک شدی!
+      </p>
+      <div className="font-normal text-[16px] text-[#17494D] mt-3">
+        <p>متاسفانه متین دیگه دوست نداره پست‌ها و</p>
+        <p>استوری‌هاش رو باهات به اشتراک بذاره.</p>
+        <p>برو دنبال دوست جدید بگرد :)</p>
+      </div>
+    </div>
+  );
+};
+
+export default BlockPage;
