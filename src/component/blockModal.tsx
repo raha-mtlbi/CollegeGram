07f396e80512a46e51c9708070054c2b9d0465ee
raha.mtlbi <raha.mtlbi@gmail.com>
2023-09-10 17:11:13 +0330

    notif collegians block modal

--- src/component/blockModal.tsx
+++ src/component/blockModal.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Dialog } from "@headlessui/react";
 import Button from "./button";
+import blockIcon from "../assets/icons/report.svg";
+import { useUser } from "../features/hooks";
+
+import profile from "../assets/icons/Ellipse.svg";
+import tik from "../assets/icons/check-circle.svg";
 
 const BlockModal = ({
   open,
@@ -9,18 +14,52 @@ const BlockModal = ({
   open: boolean;
   onClose: () => void;
 }) => {
+  const user = useUser();
   return (
-    <Dialog
-      as="div"
-      open={open}
-      onClose={onClose}
-      className="flex justify-center items-center relative z-10 "
-      style={{ direction: "rtl" }}
-    >
-      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+    <Dialog as="div" open={open} onClose={onClose} style={{ direction: "rtl" }}>
+      <div
+        className="fixed inset-0 bg-black/30 overflow-y-auto"
+        aria-hidden="true"
+      />
       <div className="fixed inset-0 overflow-y-auto">
-        <div className="flex min-h-full items-center justify-center p-4 text-center ">
-          <Dialog.Panel className="w-full  max-w-md transform overflow-hidden rounded-2xl bg-[#F3F0EE] p-6 text-left align-middle shadow-xl transition-all"></Dialog.Panel>
+        <div className="flex min-h-full items-center justify-center text-center ">
+          <Dialog.Panel className=" transform overflow-hidden rounded-3xl bg-[#FFF] shadow-xl transition-all w-[375px] h-[439px] text-[#17494D]">
+            <div className="items-center justify-center text-center">
+              <img src={blockIcon} className="mx-auto mt-[25px]" />
+              <p className="text-[22px] font-normal mt-[3px]">بلاک</p>
+            </div>
+
+            <div className="flex">
+              <div className="mr-[51px] text-right">
+                <p className="text-[13px] font-bold mt-[50px]">متین دهقان</p>
+                <p className="text-[11px] mt-[5px]">170 هزار دنبال‌کننده</p>
+              </div>
+              <div className="rounded-full w-[80px] h-[80px] mr-[116px] mt-[30px]">
+                <img src={profile} />
+                <img src={tik} className="-mt-5" />
+              </div>
+            </div>
+
+            <div className="text-[14px] text-[#17494D] text-right mr-[51px] mt-[50px]">
+              <p className="font-bold">مطمئنی میخوای متین رو بلاک کنی؟</p>
+              <p className="mt-[8px]">
+                اگه بلاکش کنی دیگه نمیتونه بهت پیام بده و
+              </p>
+              <p className="mt-[8px]">
+                پست هات رو ببینه.قابلیت لایک کردن و کامنت{" "}
+              </p>
+              <p className="mt-[8px]">
+                گذاشتن زیر پست های توهم براش مسدود میشه.
+              </p>
+            </div>
+
+            <div className="mt-[25px]">
+              <button type="submit" className="w-[104px] ml-[8px] mr-[100px]">
+                نه پشیمونم
+              </button>
+              <Button title={"آره، حتما"} width="100px" type="submit" />
+            </div>
+          </Dialog.Panel>
         </div>
       </div>
     </Dialog>
--- src/page/collegians.tsx
+++ src/page/collegians.tsx
@@ -1,5 +1,63 @@
-import SideBar from "../component/sidebar"
+import SideBar from "../component/sidebar";
+import arrow from "../assets/icons/arrow-back.svg";
+import user from "../assets/icons/person.svg";
+import dot from "../assets/icons/ellipsis.svg";
 
 export default function Collegians() {
-  return <div className="flex justify-end mt-32"></div>
+  const data = [
+    {
+      name: "یاسین",
+      followers: "دقیقه پیش",
+      profile: "",
+      photoList: [arrow, arrow, arrow, arrow],
+    },
+    {
+      name: "یاسین",
+      followers: "دقیقه پیش",
+      profile: "",
+      photoList: [arrow, arrow, arrow, arrow],
+    },
+    {
+      name: "یاسین",
+      followers: "دقیقه پیش",
+      profile: "",
+      photoList: [arrow, arrow, arrow, arrow],
+    },
+  ];
+  return (
+    <div className="flex flex-col justify-end mt-32">
+      <p></p>
+      {data.map((item) => {
+        return (
+          <div>
+            <div className="flex">
+              {item.photoList.map((photo) => {
+                return (
+                  <div className="">
+                    <img src={photo}></img>
+                  </div>
+                );
+              })}
+              <img src={arrow} />
+            </div>
+            {/* paeen */}
+            <div className="flex">
+              <div className="rounded-full w-[20px] h-[20px]">
+                <img
+                  alt="profile"
+                  src={item.profile ? item.profile : user}
+                  className="w-[40px] h-[40px]"
+                ></img>
+              </div>
+              <div>
+                <p>{item.name}</p>
+                <p>{item.followers}</p>
+              </div>
+              <img src={dot} />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
