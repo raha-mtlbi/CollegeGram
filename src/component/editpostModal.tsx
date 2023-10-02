f0b4762d4ee1d9fde5d6ddcc5c0ede3ec20c3725
faezeh.barzegar68 <faezeh.barzegar68@gmail.com>
2023-10-03 01:26:12 +0330

    fix bug

--- src/component/editpostModal.tsx
+++ src/component/editpostModal.tsx
@@ -30,7 +30,7 @@ const EditPostModal = ({
   const formik = useFormik({
     initialValues: {
       caption: caption || "",
-      closeFriend: closeFriend,
+      closeFriend: closeFriend || false,
       tags: tag?.join(" ") || [""],
     },
     enableReinitialize: true,
@@ -40,48 +40,50 @@ const EditPostModal = ({
 
   return (
     <MyModal open={open} onClose={onClose}>
-      <p className="text-center text-[20px] font-bold not-italic leading-normal my-2 ">
-        ویرایش پست
-      </p>
-      <div className="mb-5">
-        <p className="my-2 text-[#17494D] text-start">توضیحات</p>
-        <textarea
-          placeholder={caption}
-          {...formik.getFieldProps("caption")}
-          className=" w-80 p-2 h-[164px] rounded-[10px] bg-[#F3F0EE]	border border-[#17494d80] resize-none"
-        />
-      </div>
-      <div className="my-5">
-        <p className="mb-2 text-[#17494D] text-start">تگ‌ها</p>
-        <input
-          className="w-full px-2 h-[40px] rounded-[10px] bg-[#F3F0EE]	border border-[#17494d80] resize-none"
-          type="text"
-          {...formik.getFieldProps("tags")}
-        />
-      </div>
-      <div className="flex justify-start">
-        <label className="relative inline-flex items-center cursor-pointer mb-6 mt-3">
-          <span className="ml-3 text-sm font-medium text-[#17494D] ">
-            فقط نمایش به دوستان نزدیک
-          </span>
-          <input
-            type="checkbox"
-            {...formik.getFieldProps("closeFriend")}
-            className="sr-only peer"
+      <form onSubmit={formik.handleSubmit} style={{ width: 350 }}>
+        <p className="text-center text-[20px] font-bold not-italic leading-normal my-2 ">
+          ویرایش پست
+        </p>
+        <div className="mb-5">
+          <p className="my-2 text-[#17494D] text-start">توضیحات</p>
+          <textarea
+            placeholder={caption}
+            {...formik.getFieldProps("caption")}
+            className=" w-full p-2 h-[164px] rounded-[10px] bg-[#F3F0EE]	border border-[#17494d80] resize-none"
           />
-          <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none  dark:peer-focus: rounded-full peer  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white  after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-gray-800"></div>
-        </label>
-      </div>
-      <div className="flex justify-end">
-        <button className="mx-6" onClick={onClose}>
-          پشیمون شدم
-        </button>
-        {loading ? (
-          <LoadingPage />
-        ) : (
-          <Button title={"ثبت تغییرات"} type="submit" />
-        )}
-      </div>
+        </div>
+        <div className="my-5">
+          <p className="mb-2 text-[#17494D] text-start">تگ‌ها</p>
+          <input
+            className="w-full px-2 h-[40px] rounded-[10px] bg-[#F3F0EE]	border border-[#17494d80] resize-none"
+            type="text"
+            {...formik.getFieldProps("tags")}
+          />
+        </div>
+        <div className="flex justify-start">
+          <label className="relative inline-flex items-center cursor-pointer mb-6 mt-3">
+            <span className="ml-3 text-sm font-medium text-[#17494D] ">
+              فقط نمایش به دوستان نزدیک
+            </span>
+            <input
+              type="checkbox"
+              {...formik.getFieldProps("closeFriend")}
+              className="sr-only peer"
+            />
+            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none  dark:peer-focus: rounded-full peer  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white  after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-gray-800"></div>
+          </label>
+        </div>
+        <div className="flex justify-end">
+          <button className="mx-6" onClick={onClose}>
+            پشیمون شدم
+          </button>
+          {loading ? (
+            <LoadingPage />
+          ) : (
+            <Button title={"ثبت تغییرات"} type="submit" />
+          )}
+        </div>
+      </form>
     </MyModal>
   );
 };
