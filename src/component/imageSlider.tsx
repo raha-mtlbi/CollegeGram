83d3a9b7963659d4a06a69b8ce555d60bc46afd4
Mohammadreza <109216626+MohammadrezaFarahmand@users.noreply.github.com>
2023-10-07 11:37:15 +0330

    fix the height of image slider

--- src/component/imageSlider.tsx
+++ src/component/imageSlider.tsx
@@ -15,27 +15,30 @@ export default function ImageSlider({
   photoDetail: IImage[] | any
 }) {
   return (
-    <div className="md:w-full z-0">
-      <Swiper
-        slidesPerView={1}
-        pagination={true}
-        navigation={true}
-        // width={100}
-        // height={488}
-        spaceBetween={0}
-        modules={[Pagination, Navigation]}>
-        {photoDetail?.photos?.map((i: any) => {
-          return (
-            <SwiperSlide key={i}>
-              <img
-                alt="postImage"
-                src={i || sample}
-                className="w-full h-[350px] rounded-r-md z-0 object-cover"
-              />
-            </SwiperSlide>
-          )
-        })}
-      </Swiper>
+    <div className="md:w-full ">
+      <div
+        className="swiper-container "
+        style={{ height: "0", paddingBottom: "100%" }} // Create a square aspect ratio of 1:1
+      >
+        <Swiper
+          slidesPerView={1}
+          pagination={true}
+          navigation={true}
+          spaceBetween={0}
+          modules={[Pagination, Navigation]}>
+          {photoDetail?.photos?.map((i: any, index: number) => {
+            return (
+              <SwiperSlide key={index}>
+                <img
+                  alt="postImage"
+                  src={i || sample}
+                  className="w-full h-full rounded-r-md z-0 object-cover"
+                />
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </div>
     </div>
   )
 }
